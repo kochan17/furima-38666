109b72308d248bf4c2ea1d6055fc05c2abf09823
Kota <i7811832616@gmail.com>
2023-05-12 17:44:56 +0900

    新規登録画面/ログイン画面の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ApplicationRecord
 
   validates :nick_name, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-validates :password, format: { with: VALID_PASSWORD_REGEX }
-end
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
+  # Add more validations according to your requirements
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
+
+  # Add any other validations that might be required for your application
+end
