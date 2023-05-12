b965c0d02f0d091b3ef2926db34de3ae65499b8e
Kota <i7811832616@gmail.com>
2023-05-12 17:47:27 +0900

    バリデーションの明記と強化
    ・first_name および last_name には、全角ひらがな、カタカナ、漢字のみが許可
    ・first_name_kana および last_name_kana には、全角カタカナのみが許可

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 
   # Add more validations according to your requirements
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 
   # Add any other validations that might be required for your application
