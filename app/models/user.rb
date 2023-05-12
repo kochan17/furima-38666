a2d678e4027e9dd737d775153daa68de1af2f7d4
Kota <i7811832616@gmail.com>
2023-05-12 19:43:44 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,9 @@ class User < ApplicationRecord
   validates :nick_name, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-
-  # Add more validations according to your requirements
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
-
-  # Add any other validations that might be required for your application
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角で入力してください" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角で入力してください" }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :birth, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :user do
     nick_name {Faker::Name.name}
-    family_name {"太郎"}
-    first_name {"山田"}
-    family_name_kana {"タロウ"}
-    first_name_kana {"ヤマダ"}
+    family_name {"鈴木"}
+    first_name {"一郎"}
+    family_name_kana {"スズキ"}
+    first_name_kana {"イチロウ"}
     birth {Faker::Date.between(from: 10.days.ago, to: Date.today)}
     email {Faker::Internet.free_email}
     password { '1a' + Faker::Internet.password(min_length: 6) }
