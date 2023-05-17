d1df48947e2f75581d94b26d7878d69c2fd5135a
Kota <i7811832616@gmail.com>
2023-05-18 01:57:31 +0900

    payjpインストール

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,29 +1,30 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:new, :create]
   before_action :redirect_if_sold, only: [:new, :create]
   before_action :redirect_if_owner, only: [:new, :create]
 
   def new
-    @item = Item.find(params[:id])
-   @order_form = OrderForm.new
+    @order_form = OrderForm.new
   end
 
   def create
-    @order_form = OrderForm.new(order_form_params)
-    if @order_form.valid?
-      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-      Payjp::Charge.create(
-        amount: order_form_params[:price],  # 商品の値段
-        card: order_form_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
-
-      @order_form.save
-      redirect_to root_path
-    else
-      render 'new'
+    def create
+      @order_form = OrderForm.new(order_form_params)
+      if @order_form.valid?
+        Payjp.api_key = ENV[sk_test_0b93b5b828929192c0455699]
+        Payjp::Charge.create(
+          amount: order_form_params[:price],  # 商品の値段
+          card: order_form_params[:token],    # カードトークン
+          currency: 'jpy'                 # 通貨の種類（日本円）
+        )
+  
+        @order_form.save
+        redirect_to root_path
+      else
+        render 'new'
+      end
     end
-  end
 
   private
 
@@ -34,20 +35,23 @@ class OrdersController < ApplicationController
   end
 
   def set_item
-    @item = Item.find(params[:order_form][:item_id])
+    @item = Item.find(params[:id])
   end
 
   def redirect_if_sold
-    @item = Item.find(params[:id])
-    return unless PurchaseHistory.exists?(item_id: @item.id)
-
-    redirect_to root_path
+    redirect_to root_path if PurchaseHistory.exists?(item_id: @item.id)
   end
 
   def redirect_if_owner
-    return if current_user != @item.user
-
-    redirect_to root_path
+    redirect_to root_path if current_user == @item.user
   end
 
+  def charge_payment
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_form_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
