d379f3b1e602983b25b39bf9b24abd254e67e2ad
Kota <i7811832616@gmail.com>
2023-05-19 16:02:59 +0900

    ビュー修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -79,7 +79,6 @@ RSpec.describe User, type: :model do
       it 'passwordが5文字以下では登録できない' do
         @user.password = '12345'
         @user.password_confirmation = '12345'
-=======
 
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
