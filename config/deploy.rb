fb05369d1a9a8abe70568f67a4eca250e669246f
Kota <i7811832616@gmail.com>
2023-05-25 19:08:58 +0900

    deployファイルレポジトリ修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock "3.17.3"
 set :application, "furima-38666"
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, "git@example.com:kochan17/furima38666.git"
+set :repo_url, "git@github.com:kochan17/furima-38666.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
